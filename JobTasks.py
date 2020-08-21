a92eb7cf8afe816d1a1ed8ffd441b4041e7709bb
BHARAT PRATAP SINGH SHISHODIA <32167970+bharatpss@users.noreply.github.com>
2020-08-21 17:02:43 +0530

    Add files via upload

--- JobTasks.py
+++ JobTasks.py
@@ -8,6 +8,7 @@ import time
 from collections import deque
 from itertools import permutations
 
+
 '''for j in L2:
         if j not in list(que):
             if len(que) < max_size:
@@ -25,18 +26,106 @@ from itertools import permutations
             pass'''
 
 global que
-max_size = 8
+
+print("------------------------------------------------------------------------")
+print("                    Job Tasks Optimization Program:                     ")
+print("------------------------------------------------------------------------")
+print("Note:")
+#print("1. If Entered numbers are like this : 2 5 9 f 5 2 6 g 5 6.\nThen program take [2, 5, 9, 5, 2, 6, 5, 6] as an inputs.")
+'''
+try:
+    A = [int(i) for i in input("Enter number of elements : ").strip().split() if i.isdigit()]
+except ValueError:
+    print("That wasn't an integer :(")
+'''
+
+print("1. For Performing N number of jobs Program is Under Construction")
+print("            ⚠ Content will be available soon ⚠               ")
+print("------------------------------------------------------------------------")
+print("|          As of Now this Program can Optimize 6 Jobs Only.            |")
+print("|   Please enter one Task and hit enter to feed another task in Job:   |")
+print("------------------------------------------------------------------------")
+
+
+n = int(input("Enter count of tasks need to assign to job: "))
+
+#A = [int(i) for i in input("Enter number of elements : ").strip().split() if i.isdigit()]
+print("------Tasks need to be enter in Interger between (1-9)----")
+'''
+print("Enter Tasks for Job A: ")
+A = [int(input()) for _ in range(n)]
+print("Enter Tasks for Job B: ")
+B = [int(input()) for _ in range(n)]
+print("Enter Tasks for Job C: ")
+C = [int(input()) for _ in range(n)]
+print("Enter Tasks for Job D: ")
+D = [int(input()) for _ in range(n)]
+print("Enter Tasks for Job E: ")
+E = [int(input()) for _ in range(n)]
+print("Enter Tasks for Job F: ")
+F = [int(input()) for _ in range(n)]
+'''
+def Inp(n):
+    
+    b = []
+    while n > 0:
+        
+        try:
+            b.append(int(input()))
+            n -=1
+        
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+            print("try one more time")
+            #better try again... Return to the start of the loop
+            continue
+    
+    return b
+
+print("Enter Tasks for Job A: ")
+#A = [int(input()) for _ in range(n)]
+A = Inp(n)
+print("Enter Tasks for Job B: ")
+#B = [int(input()) for _ in range(n)]
+B = Inp(n)
+print("Enter Tasks for Job C: ")
+#C = [int(input()) for _ in range(n)]
+C = Inp(n)
+print("Enter Tasks for Job D: ")
+#D = [int(input()) for _ in range(n)]
+D = Inp(n)
+print("Enter Tasks for Job E: ")
+E = Inp(n)
+#E = [int(input()) for _ in range(n)]
+print("Enter Tasks for Job F: ")
+#F = [int(input()) for _ in range(n)]
+F = Inp(n)
+print("------------------------------------------------------------------------")
+
+print("Job A Taks {}".format(A))
+print("Job B Taks {}".format(B))
+print("Job C Taks {}".format(C))
+print("Job D Taks {}".format(D))
+print("Job E Taks {}".format(E))
+print("Job F Taks {}".format(F))
+
+print("------------------------------------------------------------------------")
+
+max_size = int(input("Enter Feeder size (Queue Size) "))
 que = deque(maxlen= max_size)
+
 
 class JobTask():
     "This is Job Task Management class"
     que.clear()
     count = 0
-    def __init__(self,a,b,c,d):
+    def __init__(self,a,b,c,d,e,f):
         self.a=a
         self.b=b
         self.c=c
         self.d=d
+        self.e=e
+        self.f=f
     
     def cal(self,z):
         for j in z:
@@ -68,17 +157,29 @@ class JobTask():
                 pass'''
         self.cal(self.b)
         self.cal(self.c)
-        return self.cal(self.d)#, print(self.count)
-    
+        self.cal(self.d)#, print(self.count)
+        self.cal(self.e)
+        return self.cal(self.f)
+         
     
 
-s = time.time()
-A = [2,3,4,5]
-B = [8,7,2,3]
-C = [4,5,6,7]
-D = [8,9,2,6]
+'''
+#A = [2,3,4,5]
+#B = [8,7,2,3]
+#C = [4,5,6,7]
+#D = [8,9,2,6]
+A = [2,3,4,5,6,7]
+B = [8,7,2,3,4,9]
+C = [4,5,6,7,3,8]
+D = [8,9,2,6,1,5]
+E = [5,2,6,3,4,8]
+F = [6,3,4,8,2,7]
+'''
 
-jb = JobTask([8, 9, 2, 6], [4, 5, 6, 7], [8, 7, 2, 3], [2, 3, 4, 5])
+
+
+
+'''jb = JobTask([8, 9, 2, 6], [4, 5, 6, 7], [8, 7, 2, 3], [2, 3, 4, 5])
 
 print(jb.opt_function())
 print(jb.a, jb.b, jb.c, jb.d, list(que))
@@ -87,16 +188,64 @@ e = time.time()
 print(e-s)
 del jb
 
-'''permList = permutations([A,B,C,D])
-
-# print all permutations 
-for perm in list(permList):
+'''
+s = time.time()
+permList = permutations([A,B,C,D,E,F])
+permString = permutations("ABCDEF")
+result = []
+# print all permutations
+for perm, stri in zip(list(permList), list(permString)):
     #print(perm)
-    jb = JobTask(perm[0], perm[1], perm[2], perm[3])
-    result = jb.opt_function()
-    if result == 2:
-        print(perm)
-    else:
-        pass
-    del jb'''
-        
+    x=[]
+    jb = JobTask(perm[0], perm[1], perm[2], perm[3], perm[4], perm[5])
+    r = jb.opt_function()
+    x.append(stri)
+    x.extend(perm)
+    x.append(r)
+    result.append(x)
+    #print("DeQueue", que)
+    #print('Count of operations', result)
+    #print(perm)
+    #if result == 2:
+    #   print(perm)
+    #else:
+    #   pass
+    del jb
+
+e = time.time()
+print("-----------------------------------------------------------------------")
+print("Time Taken tO Execute :")
+print(e-s, "Sec")
+
+import pandas as pd
+pd.set_option("display.max_columns", None)
+
+
+dataFrame = pd.DataFrame(result,)
+DF = pd.DataFrame()
+
+DF['Permutations'] = dataFrame[0]
+DF['JobSeq1'] = dataFrame[1]
+DF['JobSeq2'] = dataFrame[2]
+DF['JobSeq3'] = dataFrame[3]
+DF['JobSeq4'] = dataFrame[4]
+DF['JobSeq5'] = dataFrame[5]
+DF['JobSeq6'] = dataFrame[6]
+DF['Count_of_Operations'] = dataFrame[7]
+
+#print("Result in Table formate")
+#print(DF.sort_values(by=['Count_of_Operations']))
+print("\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/")
+print("|                        Result LookUp                             |")
+print("/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\")
+print("Less then 10 Operations Results (Can or Can not be present)")
+print(DF.sort_values(by=['Count_of_Operations']).head(10))
+#dataFrame.to_csv("myFile.csv")
+#install Auto py to Exe
+
+#time.sleep(500)
+#time.sleep(500)
+n = input("Press Any key to exit: ")
+
+
+#dataFrame.to_csv("myFileofJobTasksfile.csv")     
--- untitled0.py
+++ untitled0.py
@@ -1,29 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug 20 09:28:46 2020
+Created on Fri Aug 21 11:03:38 2020
 
 @author: 20TE147
 """
-
 import time
 from collections import deque
 from itertools import permutations
 import pandas as pd
 pd.set_option("display.max_columns", None)
-#pd.set_option("display.max_rows", None)
 
-'''
-A = [2,3,4,5,6,7]
-B = [8,7,2,3,4,9]
-C = [4,5,6,7,3,8]
-D = [8,9,2,6,1,5]
-E = [5,2,6,3,4,8]
-F = [6,3,4,8,2,7]
-'''
-#n = int(input("Enter number of elements : "))
-
-#user_input = raw_input("Please enter five numbers separated by a single space only: ")
-#input_numbers = [int(i) for i in input("Enter number of elements : ").strip().split(' ') if i.isdigit()]
 print("------------------------------------------------------------------------")
 print("                    Job Tasks Optimization Program:                     ")
 print("------------------------------------------------------------------------")
@@ -43,23 +29,48 @@ print("|          As of Now this Program can Optimize 6 Jobs Only.            |"
 print("|   Please enter one Task and hit enter to feed another task in Job:   |")
 print("------------------------------------------------------------------------")
 
-
 n = int(input("Enter count of tasks need to assign to job: "))
 
 #A = [int(i) for i in input("Enter number of elements : ").strip().split() if i.isdigit()]
-print("Tasks need to be enter in Interger between (1-9)")
+print("------Tasks need to be enter in Interger between (1-9)----")
+#print("Enter Tasks for Job A: ")
+
+def Inp(n):
+    
+    b = []
+    while n > 0:
+        
+        try:
+            b.append(int(input()))
+            n -=1
+        
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+            print("Enter correct serise at one time")
+            #better try again... Return to the start of the loop
+            continue
+    
+    return b
+
+
 print("Enter Tasks for Job A: ")
-A = [int(input()) for _ in range(n)]
+#A = [int(input()) for _ in range(n)]
+A = Inp(n)
 print("Enter Tasks for Job B: ")
-B = [int(input()) for _ in range(n)]
+#B = [int(input()) for _ in range(n)]
+B = Inp(n)
 print("Enter Tasks for Job C: ")
-C = [int(input()) for _ in range(n)]
+#C = [int(input()) for _ in range(n)]
+C = Inp(n)
 print("Enter Tasks for Job D: ")
-D = [int(input()) for _ in range(n)]
+#D = [int(input()) for _ in range(n)]
+D = Inp(n)
 print("Enter Tasks for Job E: ")
-E = [int(input()) for _ in range(n)]
+E = Inp(n)
+#E = [int(input()) for _ in range(n)]
 print("Enter Tasks for Job F: ")
-F = [int(input()) for _ in range(n)]
+#F = [int(input()) for _ in range(n)]
+F = Inp(n)
 
 print("------------------------------------------------------------------------")
 
@@ -72,41 +83,14 @@ print("Job F Taks {}".format(F))
 
 print("------------------------------------------------------------------------")
 '''
-A = [int(i) for i in input("Enter number of elements : ").strip().split() if i.isdigit()]
-B = list(map(int,input("\nEnter the numbers for B: ").strip().split()))[:n]
-C = list(map(int,input("\nEnter the numbers for C: ").strip().split()))[:n]
-D = list(map(int,input("\nEnter the numbers for D: ").strip().split()))[:n]
-E = list(map(int,input("\nEnter the numbers for E: ").strip().split()))[:n]
-F = list(map(int,input("\nEnter the numbers for F: ").strip().split()))[:n]
-'''
-
 max_size = int(input("Enter Feeder size (Queue Size) "))
 
 
 
 que = deque(maxlen= max_size)
 
-
-
-#a_set = set(A)
-#b_set = set(B)
-#c_set = set(C)
-
-'''if len(a_set.intersection(b_set)) > 0: 
-    print(a_set.intersection(b_set))   
-else: 
-    print("no common elements") 
-    
-def cal(a):
-    for j in a:
-        if j not in list(que.queue):
-            que.put(j)
-        else:
-            pass
-
- 
-'''
 def opt_function(L1,L2,L3,L4,L5,L6):
+    
     que.clear()
     count = 0
     #print('Queue',list(que))
@@ -214,13 +198,9 @@ def opt_function(L1,L2,L3,L4,L5,L6):
         
             pass
     #print('Queue',list(que))
-    #print(count)
+    print(count)
     return(count)
-        
-        
-#opt_function([5, 2, 6, 3, 4, 8],	[2, 3, 4, 5, 6, 7],	[4, 5, 6, 7, 3, 8],	[6, 3, 4, 8, 2, 7],	[8, 7, 2, 3, 4, 9],	[8, 9, 2, 6, 1, 5])
-    
-             
+
 s = time.time()
 #import string
 permList = permutations([A,B,C,D,E,F])
@@ -237,22 +217,13 @@ for perm, stri in zip(list(permList), list(permString)):
     result.append(x)
     
     #if r <= 5:
-        #print(perm)
-        #print(stri)
+     #   print(perm)
+      #  print(stri)
     #else:
-        #pass
-             
-#print(a_set.union(b_set))
-#que.put(a_set.union(b_set))
-#print(list(que.queue))
-#new = a_set.union(b_set)
-        
-#print(result)
+     #   pass
     
 
 e = time.time()
-print("-----------------------------------------------------------------------")
-print("Time Taken tO Execute :")
 print(e-s)
 
 
@@ -273,8 +244,7 @@ DF['Count_of_Operations'] = dataFrame[7]
 
 #print("Result in Table formate")
 #print(DF.sort_values(by=['Count_of_Operations']))
-print("\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/")
-print("/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\")
+
 print("Less then 10 Operations Results (Can or Can not be present)")
 print(DF[DF["Count_of_Operations"] < 10].sort_values(by=['Count_of_Operations']).head(10))
 #dataFrame.to_csv("myFile.csv")
@@ -282,6 +252,6 @@ print(DF[DF["Count_of_Operations"] < 10].sort_values(by=['Count_of_Operations'])
 
 #time.sleep(500)
 #time.sleep(500)
-n = input("Press Any key to exit")
+n = input("Press something to exit")
 
-
+'''
